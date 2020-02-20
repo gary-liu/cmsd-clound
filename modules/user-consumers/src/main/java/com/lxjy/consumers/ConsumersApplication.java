77aec368b7babc4745491069ae5d5c505beb0d71
gary-liu <498982703@qq.com>
2020-02-20 21:10:06 +0800

    aa

---
+++ modules/user-consumers/src/main/java/com/lxjy/consumers/ConsumersApplication.java
@@ -0,0 +1,23 @@
+package com.lxjy.consumers;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableCircuitBreaker
+@EnableFeignClients
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumersApplication.class);
+    }
+
+}
